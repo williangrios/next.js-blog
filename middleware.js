e31b7105ceb631c86562dd42d7dfbe40ea8e7727
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-12-15 08:06:07 -0300

    feat: add cors

--- middleware.js
+++ middleware.js
@@ -17,6 +17,10 @@ export const config = {
 export default withAuth(async function middleware(req) {
     const url = req.nextUrl.pathname
     const userRole = req?.nextauth?.token?.user?.role
+    // cors - allow other domains to access API
+    if (url.includes('/api')) {
+        NextResponse.next().headers.append('Access-Control-Allow-Origin', '*')
+    }
     if (url?.includes('/admin') && userRole !== 'admin'){
         return NextResponse.redirect(new URL('/', req.url))
     }
