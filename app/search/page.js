9ecd241bd5b8ad4f974c454f9081af311522a2a6
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-12-15 08:06:21 -0300

    feat: finish layout

--- app/search/page.js
+++ app/search/page.js
@@ -1,36 +1,42 @@
-'use client'
+"use client";
 
-
-
-import React, { useEffect } from 'react'
-import { useSearch } from "../../context/search"
-import { useSearchParams } from 'next/navigation'
-import BlogList from '../../components/blogs/BlogList'
+import React, { useEffect } from "react";
+import { useSearch } from "../../context/search";
+import { useSearchParams } from "next/navigation";
+import BlogList from "../../components/blogs/BlogList";
 
 function Search() {
-  const {setSearchQuery, searchResults, setSearchResults} = useSearch()
-  const searchParams = useSearchParams()
-  const query = searchParams.get('searchQuery')
+  const { setSearchQuery, searchResults, setSearchResults } = useSearch();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("searchQuery");
 
-    useEffect(() => {
+  useEffect(() => {}, [query]);
 
-
-      
-    }, [query])
-
-    const fetchResultsOnLoad = async () => {
-      try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API}/search? `)
-      } catch (error) {
-        
+  const fetchResultsOnLoad = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API}/search?searchQuery${query}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch search results");
       }
+      const data = await response.json();
+      setSearchResults(data);
+    } catch (error) {
+      console.log(error);
     }
-    
+  };
+
   return (
-    <div>
-      <BlogList/>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <p className="lead">Search Results {searchResults.length}</p>
+          {searchResults ? <BlogList blogs={searchResults} /> : ""}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
