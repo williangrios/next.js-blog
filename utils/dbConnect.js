d3364641b757fcc9613a453536a56ea903e19496
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-12-15 07:43:28 -0300

    feat: add dbConnect

---
+++ utils/dbConnect.js
@@ -0,0 +1,11 @@
+const { default: mongoose } = require("mongoose")
+
+
+const dbConnect = async () => {
+    if (mongoose.connection.readyState >= 1 ){
+        return
+    }
+    mongoose.connect(process.env.DB_URI)
+}
+
+export default dbConnect
